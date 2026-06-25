9b21ad3080b2caa7a1c61d4d31e82056a3f5622b
Ruslan Samsonovych <arankutakepakuta@gmail.com>
2026-06-25 20:30:58 +0300

    implement Cinema Visit

--- app/main.py
+++ app/main.py
@@ -5,10 +5,10 @@ from app.people.customer import Customer
 
 
 def cinema_visit(
+    movie: str,
     customers: list[dict[str, str]],
     hall_number: int,
-    cleaner: str,
-    movie: str
+    cleaner: str
 ) -> None:
     customers_instance = [
         Customer(name=customer["name"], food=customer["food"])
